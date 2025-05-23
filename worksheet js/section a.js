056eb15596b7f2cc7d32bb2600f895bd70a5fcee
Gurvinder50829 <gurvindersain268@gmail.com>
2025-05-23 13:24:35 +0530

    update

---
+++ worksheet js/section a.js
@@ -0,0 +1,16 @@
+ let str="alphaintern ";
+console.log(str.trim().toUpperCase());
+//answer 
+ 
+let str1 ="webdevelopment";
+ console.log(str1.slice(3,6));
+ 
+ let str3="AlphaIntern";
+ console.log(str3.substring(6));
+
+ let str4="Welcome to AlphaIntern";
+ console.log(str4.includes("Alpha"));
+ 
+ let char ="A";
+ console.log(char.charCodeAt(0));
+ 
---
+++ worksheet js/section b .js
@@ -0,0 +1,6 @@
+//Hello Riya, you scored 92 in Web Development! Print this statement  using template literals
+name="riya";
+score="92";
+subject="Web Devolpment";
+console.log("Hello  Riya, you scored 92 in Web Development! ");
+
